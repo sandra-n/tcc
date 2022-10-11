8b942fba84af3cb1bae5cb33f2a5528e9e009c95
Sane08 <gsanefuji@gmail.com>
2022-10-10 21:23:14 -0300

    using cursor function to search mentions of bot/user and replying to them

--- bots/test_bot/main.py
+++ bots/test_bot/main.py
@@ -1,6 +1,7 @@
 import tweepy
 import logging
 import twitter_keys
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -30,31 +31,40 @@ def tweet_message(api, message, image_path=None):
 
     print('tweeted successfully :D')
 
-# Searches mention from own tweets and responds
+# Searches mention from tweets and responds using cursor
 def searches_mentions(api, keywords, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        if tweet.in_reply_to_status_id is not None:
+        if tweet.in_reply_to_status_id is None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
             logger.info('Answering to %s', tweet.user.name)
 
-            if not tweet.user.following:
-                tweet.user.follow()
-
-            api.update_status(
-                status="Please reach us via DM",
-                in_reply_to_status_id=tweet.id,
-            )
+            original_tweet_id = tweet.in_reply_to_status_id
+            original_tweet = api.get_status(original_tweet_id)
+            logger.info("tweet: %s", original_tweet.text)
+    
+            api.update_status(status="Some answer", in_reply_to_status_id=tweet.id,  auto_populate_reply_metadata=True)
     return new_since_id
 
+class IDPrinter(tweepy.StreamingClient):
+
+    def on_tweet(self, tweet):
+        print(tweet.id)
 
 def main():
     api = create_api()
-    tweet_message(api, 'This is a test')
-
+    bot_id = api.verify_credentials().id
+    since_id = 1
+    while True:
+        since_id = searches_mentions(api, ["help", "support", "calling"], since_id, bot_id)
+        logger.info("Waiting...")
+        time.sleep(60)
+    #tweet_message(api, 'test')
+    #printer = IDPrinter(twitter_keys.bearer_token)
+    #printer.sample()
 
 if __name__ == '__main__':
     main()
