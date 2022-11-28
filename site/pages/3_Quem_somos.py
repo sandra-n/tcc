f9613b5fd0cdbee7a12ad168fe8fb5e5a4a81201
Sane08 <gsanefuji@gmail.com>
2022-11-27 22:25:35 -0300

    Adicionadas funções para reprodução de vídeo

--- site/pages/3_Quem_somos.py
+++ site/pages/3_Quem_somos.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
 
 st.title('Quem somos')
 
@@ -7,5 +12,9 @@ st.header('Integrantes')
 st.header('Professor orientador')
 
 st.header('Vídeo')
+video_file = open("video/MyGameList.mp4", "rb")
+video_bytes = video_file.read()
+st.video(video_bytes)
+st.video(data="https://www.youtube.com/watch?v=7jHcszxmKRw", format="video/mp4", start_time=0)
 
 st.header('Twitter bot')
