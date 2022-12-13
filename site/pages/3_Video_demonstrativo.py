bccbeb8095e99e6c67530ae173d32f2f445f4c21
Sane08 <gsanefuji@gmail.com>
2022-12-13 16:08:52 -0300

    Retirado video teste

--- site/pages/3_Video_demonstrativo.py
+++ site/pages/3_Video_demonstrativo.py
@@ -15,7 +15,7 @@ with open(str(parent_dir) + '/../.streamlit/style.css') as f:
 st.title('Vídeo demonstrativo')
 st.write('O vídeo apresentado a seguir contém a explicação contextual do projeto assim como uma demonstração prática dos modelos implementados e do bot desenvolvido:')
 
-video_file = open(parent_dir + "/video/MyGameList.mp4", "rb")
-video_bytes = video_file.read()
-st.video(video_bytes)
-st.video(data="https://www.youtube.com/watch?v=7jHcszxmKRw", format="video/mp4", start_time=0)
+# video_file = open(parent_dir + "/video/MyGameList.mp4", "rb")
+# video_bytes = video_file.read()
+# st.video(video_bytes)
+# st.video(data="https://www.youtube.com/watch?v=7jHcszxmKRw", format="video/mp4", start_time=0)
