56bf228eef59331c55a1f279078ff464483ee4be
Sane08 <gsanefuji@gmail.com>
2022-12-19 18:48:28 -0300

    Alterado link para pagina do bot

--- site/pages/4_Quem_somos.py
+++ site/pages/4_Quem_somos.py
@@ -72,7 +72,7 @@ st.write('Acompanhe as postagens do nosso bot [@AreYou_Racist](https://twitter.c
 
 components.html(
     """
-        <a class="twitter-timeline" data-width="400" data-height="600" href="https://twitter.com/Anitta?ref_src=twsrc%5Etfw">Tweets by Anitta</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <a class="twitter-timeline" data-width="400" data-height="600" href="https://twitter.com/AreYou_Racist">Tweets by Anitta</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     """,
     height=500,
 )
